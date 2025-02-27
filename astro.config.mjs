5c70c0c9aa455278dd60baaad8311cb31ea9fa95
Ramses Angles <ramses.angles@hey.com>
2025-02-27 11:37:24 -0600

    added barndominuim landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,21 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-    site: 'https://builtecusa.com', // Replace with your site URL
-    base: '/',
-    output: 'static',
-    integrations: [
-        tailwind({ applyBaseStyles: false }),
-        react()
-    ],
-    build: {
-        assets: 'assets'
+  site: 'https://builtecusa.com',
+  base: '/',
+  output: 'static',
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    react()
+  ],
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+    routing: {
+      prefixDefaultLocale: false
     }
+  },
+  build: {
+    assets: 'assets'
+  }
 });
